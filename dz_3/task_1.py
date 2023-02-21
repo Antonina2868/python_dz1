b9d37b19901e125805b9fe5becf22b2672f8c404
antonina2868 <bonov2868@gmail.com>
2023-02-21 21:08:09 +0300

    task_1 done

--- dz_3/task_1.py
+++ dz_3/task_1.py
@@ -5,20 +5,18 @@
 # Введите номер месяца: 10
 # Результат через список: Осень
 # Результат через словарь: Осень
-# """
-#
-# """
-# year_month = input("Ведите номер месяца: ")
-# # year_month = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-# for i in year_month:
-#     if year_month == 12:
-#         print("зима")
-    # elif year_month == 1:
-    #     print("зима")
-    # elif year_month == 2:
-    #     print("зима")
 
 user_month = int(input("Ведите номер месяца: "))
+year_month = [[12, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]
+season_names = ["winter", "spring", "summer", "autumn"]
+i = 0
+
+for seasons in year_month:
+    for month_number in seasons:
+        if user_month == month_number:
+            print(season_names[i])
+    i += 1
+
 seasons = {"winter": [12, 1, 2], "spring": [3, 4, 5], "summer": [6, 7, 8], "autumn": [9, 10, 11]}
 
 for season in seasons:
