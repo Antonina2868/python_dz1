01da904d24badcd533814204d5ac63139a760734
antonina2868 <bonov2868@gmail.com>
2023-02-25 14:39:16 +0500

    cycle works

--- dz_3/task_3.py
+++ dz_3/task_3.py
@@ -19,20 +19,21 @@
 # “количества”: [5, 2, 7],
 # “ед”: [“шт.”]
 # }
+products = []
 quest = "Y"
-while quest == "Y":
+while quest.lower() == "y":
     n = int(input("Введите номер товара: "))
     prod_name = input("Введите название товара: ")
     prod_price = int(input("Введите стоимость товара: "))
     prod_quantity = int(input("Введите количество товара: "))
     prod_measure = input("Введите единицу измерения товара: ")
 
-    products =  [(n, {
+    products.append((n, {
         "название": prod_name,
         "цена": prod_price,
         "количество": prod_quantity,
         "ед": prod_measure,
-    })]
+    }))
     quest = input("Добавить товар?(Y/N)")
 
 print(products)
